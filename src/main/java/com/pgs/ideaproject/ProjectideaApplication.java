79802aa0c04bce6901b3e2ddee22961d4b5855a2
Jagoda Los (PGS Software) <jlos@pgs-soft.com>
2017-04-04 15:00:40 +0200

    Initial Commit

---
+++ src/main/java/com/pgs/ideaproject/ProjectideaApplication.java
@@ -0,0 +1,12 @@
+package com.pgs.ideaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectideaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectideaApplication.class, args);
+	}
+}
